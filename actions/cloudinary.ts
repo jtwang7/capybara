96a40e277eceee0d989d0e52a9ff9f15cb295f79
wangjintian <wangjintian@bytedance.com>
2024-10-07 02:17:33 +0800

    fix: remove lib/cloudinary

--- actions/cloudinary.ts
+++ actions/cloudinary.ts
@@ -1,6 +1,13 @@
 "use server";
 
-import { cloudinary } from "@/lib/cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 // transform image to fill parent container (full width & keep aspect ratio)
 export async function transformImageUrl({
--- actions/url-parse-action.ts
+++ actions/url-parse-action.ts
@@ -1,7 +1,14 @@
 "use server";
 
-import { cloudinary } from "@/lib/cloudinary";
 import puppeteer from "puppeteer";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 export async function urlParseAction({
   url,
